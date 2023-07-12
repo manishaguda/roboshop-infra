15f8c02514e9f62bd9ba1e4c3a3fdd88ec4ccc9c
manisha guda <manishavguda@gmail.com>
2023-07-12 18:41:14 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+module "network" {
+  source = "https://github.com/manishaguda/tf-module-vpc.git"
+
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
 }
