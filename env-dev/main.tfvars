fc53d35bcdd0cc452778b5ed74280095f45f1529
manisha guda <manishavguda@gmail.com>
2023-07-17 11:29:48 +0530

    terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,8 +4,9 @@ default_vpc_id = "vpc-03d4e63006cb172ca"
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-    public_subnets_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
+    public_subnets_cidr  = ["10.0.0.0/24", "10.0.1.0/24"]
     private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
+    availability_zones   = ["us-east-1a", "us-east-1b"]
   }
 }
 
--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "network" {
   source = "github.com/manishaguda/tf-module-vpc"
-  env            = var.env
-  default_vpc_id = var.default_vpc_id
+  env                  = var.env
+  default_vpc_id       = var.default_vpc_id
 
-  for_each       = var.vpc
-  cidr_block     = each.value.cidr_block
-  public_subnets_cidr = each.value.public_subnets_cidr
+  for_each             = var.vpc
+  cidr_block           = each.value.cidr_block
+  public_subnets_cidr  = each.value.public_subnets_cidr
   private_subnets_cidr = each.value.private_subnets_cidr
+  availability_zones   = each.value.availability_zones
 }
