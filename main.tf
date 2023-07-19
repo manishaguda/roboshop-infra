bd91647c79070e6b7e9154556284a60e5cf6acaf
manisha guda <manishavguda@gmail.com>
2023-07-19 22:16:19 +0530

    terraform

--- main.tf
+++ main.tf
@@ -6,7 +6,9 @@ module "vpc" {
   for_each   = var.vpc
   cidr_block = each.value.cidr_block
   subnets    = each.value.subnets
-
+  public_subnets = each.value.public_subnets
+#  private_subnets = each.value.private_subnets
+  availability_zones   = each.value.availability_zones
 }
 
 #  public_subnets_cidr  = each.value.public_subnets_cidr
